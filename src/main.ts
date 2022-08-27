b508988fed59891f87cfb0326951be7840857d9d
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-27 11:18:30 -0500

    Crear tarea

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import './style.css'
 
 const taskForm = document.querySelector<HTMLFormElement>("#task-form")!
 
+interface Task {
+	title: string,
+	description: string
+}
+
+let tasks: Task[] = []
+
 taskForm.addEventListener("submit", event => {
 	event.preventDefault()
 
 	const title = taskForm["title"] as unknown as HTMLInputElement
 	const description = taskForm["description"] as unknown as HTMLTextAreaElement
 
-	console.log(title.value)
-	console.log(description.value)
+	tasks.push({
+		title: title.value,
+		description: description.value
+	})
+
+	localStorage.setItem("tasks", JSON.stringify(tasks))
+
 })
