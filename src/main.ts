69c1c0db9de5fc7bfbb0e33dfe89c0bc21e32ff2
Luis Ariza <luismanuelariza@hotmail.com>
2022-08-27 11:08:26 -0500

    Estilos

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,13 @@
 import './style.css'
 
+const taskForm = document.querySelector<HTMLFormElement>("#task-form")!
+
+taskForm.addEventListener("submit", event => {
+	event.preventDefault()
+
+	const title = taskForm["title"] as unknown as HTMLInputElement
+	const description = taskForm["description"] as unknown as HTMLTextAreaElement
+
+	console.log(title.value)
+	console.log(description.value)
+})
